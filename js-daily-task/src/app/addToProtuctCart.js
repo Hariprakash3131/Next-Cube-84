5b0f712553a0d61d81d4c4a46ef3e2f805f18949
Hariprakash3131 <hariprakash3131@gmail.com>
2026-05-19 17:13:10 +0530

    react-71

--- js-daily-task/src/app/addToProtuctCart.js
+++ js-daily-task/src/app/addToProtuctCart.js
@@ -1,20 +1,20 @@
-// let cart = []
+let cart = []
 
-// function addCart(name, price, color) {
-//     let product = {
-//         ProductName: name,
-//         ProductPrice: price,
-//         ProductColor: color
-//     }
-//     cart.push(product)
-//     console.log(cart)
-// }
+function addCart(name, price, color) {
+    let product = {
+        ProductName: name,
+        ProductPrice: price,
+        ProductColor: color
+    }
+    cart.push(product)
+    console.log(cart)
+}
 
-// addCart('Shirt', 500, 'pink')
-// addCart('Shoes', 8900, 'black')
-// addCart('Watch', 600, 'White')
+addCart('Shirt', 500, 'pink')
+addCart('Shoes', 8900, 'black')
+addCart('Watch', 600, 'White')
 
-// console.log(cart[1].ProductColor)
+console.log(cart[1].ProductColor)
 
-// console.log(cart.length)
+console.log(cart.length)
 
