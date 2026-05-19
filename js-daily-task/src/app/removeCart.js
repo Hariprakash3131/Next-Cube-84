d502a1be94e6f392406a1d817d7da7faa9b78fba
Hariprakash3131 <hariprakash3131@gmail.com>
2026-05-19 17:40:44 +0530

    react-73

--- js-daily-task/src/app/removeCart.js
+++ js-daily-task/src/app/removeCart.js
@@ -1,16 +1,43 @@
-let cart=[
-    {id:1,name:"Phone"},
+// let cart=[
+//     {id:1,name:"Phone"},
+//     {
+//         id:2,
+//         name:'laptop'
+//     }
+// ]
+
+// function removeCart(ids){
+//     cart=cart.filter((remove)=>
+//         remove.id!==ids)
+//         console.log(cart)
+// }
+
+// removeCart(2)
+
+
+
+let product=[
     {
+        id:1,
+        name:"Tab",
+        price:90000
+    }
+    ,{
         id:2,
-        name:'laptop'
+        name:"Laptop",
+        price:60000
+    },
+    {
+        id:3,
+        name:"Shoes",
+        price:800
     }
 ]
 
-function removeCart(id){
-    cart=cart.filter((remove)=>
-        remove.id!==id  )
-        console.log(cart)
-  
+
+function removeProduct(name){
+    productz=product.filter((removeP)=>removeP.name!==name)
+    console.log(productz)
 }
 
-removeCart(2)
+removeProduct("Laptop")
