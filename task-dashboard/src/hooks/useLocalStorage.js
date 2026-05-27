106f81b96db384f9eb5c3736759bb43ac9af4d5c
Hariprakash3131 <hariprakash3131@gmail.com>
2026-05-27 14:12:47 +0530

    react-87

--- task-dashboard/src/hooks/useLocalStorage.js
+++ task-dashboard/src/hooks/useLocalStorage.js
@@ -1,1 +1,28 @@
-export
+import { useState,useEffect } from "react";
+
+export default function useLocalStorage(key,initialValue){
+    const [storedValue,setStoredValue]=useState(()=>{
+          if(typeof window==='undefined'){
+            return initialValue
+          }
+          const item=localStorage.getItem(key)
+          return item ? JSON.parse(item):initialValue
+    })
+
+    useEffect(()=>{
+        localStorage.setItem(key,JSON.stringify(storedValue))
+    },[key,storedValue])
+
+    return[storedValue,setStoredValue]
+}
+
+
+// Important Next.js Point
+
+// This line:
+
+// typeof window === "undefined"
+
+// is VERY important in Next.js.
+
+// Because localStorage only exists in browser, not server.
