845a2da3926f6ea5c87e81b0233d06de1e8da9df
Hariprakash3131 <hariprakash3131@gmail.com>
2026-05-28 23:24:24 +0530

    react-100

--- useref-hook/src/app/page.js
+++ useref-hook/src/app/page.js
@@ -1,12 +1,13 @@
 import Focus from "./components/inputFocus";
 import TextArea from "./components/textareaClear";
 
-
+import Timer from "./components/createTimer";
 export default function Home() {
   return (
     // <InputFocus/>
     // <Counter/>
     // <Focus/>
-    <TextArea/>
+    // <TextArea/>
+    <Timer/>
   );
 }
